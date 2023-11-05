4a9e42c8a8e52b40b1cced4d0799a2403a6cac8e
Alicja Chaszczewicz <alicja@cs.stanford.edu>
2023-11-05 13:27:10 -0800

    Add tutorial.

---
+++ Instructions.md
@@ -0,0 +1,6 @@
+1. Fork this repository
+2. Clone your forked repository
+3. Create a new virtual environment
+4. `pip install streamlit`
+5. `pip install openai`
+6. Run the app! `streamlit run app.py`
---
+++ app.py
@@ -0,0 +1,24 @@
+# example streamlit app
+from blocks import *
+
+
+def main():
+    st.title("HPI Data Annotation Tool")
+    st.video('https://www.youtube.com/watch?v=rg6CiPI6h2g&pp=ygUMcG9rZW1vbiBzb25n')
+    st.balloons()
+
+    # data = load_data()
+
+    # select_favourite_pokemon(data)
+    # select_favourite_pokemon_centered(data)
+
+    # select_favourite_pokemon_types(data)
+
+    # create_form(data)
+
+    # chat_with_pokemon_bad(data)
+    # chat_with_pokemon(data)
+
+
+if __name__ == '__main__':
+    main()
